d4722991117d90c856a8b49756301382a1ab1dc0
Gopal Metro <gopal.metro@gmail.com>
2014-07-17 23:09:26 -0400

    Changing variable scope
    Attempting to assign serial date to  noteByte

--- MIDIBlink/MIDIBlink/MIDIBlink.ino
+++ MIDIBlink/MIDIBlink/MIDIBlink.ino
@@ -8,21 +8,22 @@
 // Pin 13 has an LED connected on most Arduino boards.
 // give it a name:
 int led = 13;
+byte commandByte;
+byte noteByte;
+byte velocityByte;
+byte noteOn = 144;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
   pinMode(led, OUTPUT);
+  digitalWrite(led,LOW);
   Serial.begin(38400);
 }
 
 
 // the loop routine runs over and over again forever:
 void loop() {
-  byte commandByte;
-  byte noteByte;
-  byte velocityByte;
-  byte noteOn = 0x90;
   
   do{
     if (Serial.available()){
