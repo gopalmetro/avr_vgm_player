1f71928c8fa889128f6c1cc1cd736fca09cb4e54
Cleon Chick <cmchick+dvcs@gmail.com>
2014-07-28 04:59:46 -0400

    OOP, multi-channel, slot tracking, etc

--- Trahagean/Trahagean.ino
+++ Trahagean/Trahagean.ino
@@ -23,121 +23,232 @@
 // PB2 PB3 as YM 6-7
 #define BDATA(d) ((d >> 4) & B00001100)
 
-byte statusByte;
-byte statusCommand;
-byte MIDIChannel;
-byte databyte1; //Register and offset
-byte databyte2; //Actual value
-byte algorithm;
-uint16_t pitch;
-byte pitchMSB;
-byte pitchLSB;
+#define YM2612_OP1 B0001
+#define YM2612_OP2 B0010
+#define YM2612_OP3 B0100
+#define YM2612_OP4 B1000
+
 
 void blinkTest(int numBlinks = 1, int LEDHighTime = 50, int LEDLowTime = 50) {
-  for (int i = 0; i < numBlinks; i++) {
-    digitalWrite(13,HIGH);
-    delay(LEDHighTime);
-    digitalWrite(13,LOW);
-    delay(LEDLowTime);
-  }
+	for (int i = 0; i < numBlinks; i++) {
+		digitalWrite(13,HIGH);
+		delay(LEDHighTime);
+		digitalWrite(13,LOW);
+		delay(LEDLowTime);
+	}
 }
 
-static void write_ym(uint8_t data) {
+class YM2612 {
 
-	YM_CTRL_PORT &= ~_BV(YM_CS); // CS LOW
-	//YM_DATA_PORT = data;
-        YM_DATA_PORT = DDATA(data) | (YM_DATA_PORT & B00000011);
-        PORTB = BDATA(data) | (PORTB & B11110011);
-	_delay_us(1);
-	YM_CTRL_PORT &= ~_BV(YM_WR); // Write data
-	_delay_us(5);
-	YM_CTRL_PORT |= _BV(YM_WR);
-	_delay_us(5);
-	YM_CTRL_PORT |= _BV(YM_CS); // CS HIGH
-}
+    byte slotOperators[6]; // bit-mask to identify operators in algorithm "slots", per channel
 
-static void setreg(uint8_t reg, uint8_t data) {
-	YM_CTRL_PORT &= ~_BV(YM_A0); // A0 low (select register)
-	write_ym(reg);
-	YM_CTRL_PORT |= _BV(YM_A0);  // A0 high (write register)
-	write_ym(data);
-}
+    void write_ym(uint8_t data) {
+	    YM_CTRL_PORT &= ~_BV(YM_CS); // CS LOW
+	    YM_DATA_PORT = DDATA(data) | (YM_DATA_PORT & B00000011);
+	    PORTB = BDATA(data) | (PORTB & B11110011);
+	    _delay_us(1);
+	    YM_CTRL_PORT &= ~_BV(YM_WR); // Write data
+	    _delay_us(5);
+	    YM_CTRL_PORT |= _BV(YM_WR);
+	    _delay_us(5);
+	    YM_CTRL_PORT |= _BV(YM_CS); // CS HIGH
+    }
 
-void setup(){
-  /* activate MIDI Serial input - Gopal */
-  Serial.begin(38400);
-  pinMode(13,OUTPUT);
-  digitalWrite(13,LOW);
-  
-/* Pins setup */
-	YM_CTRL_DDR |= _BV(YM_IC) | _BV(YM_CS) | _BV(YM_WR) | _BV(YM_RD) | _BV(YM_A0) | _BV(YM_A1);
-	//YM_DATA_DDR = 0xFF;
+
+    void setreg(uint8_t reg, uint8_t data, byte bank) {
+	    if (bank) {
+	        YM_CTRL_PORT |= _BV(YM_A1); // A1 high (bank 1, YM channels 456)
+            YM_CTRL_PORT &= ~_BV(YM_A0); // A0 low (select register)
+        }
+	    else
+	        YM_CTRL_PORT &= ~(_BV(YM_A1) | _BV(YM_A0)); // A0 low (select register), A1 low (bank 0, global and YM channels 123)
+	    
+	    write_ym(reg);
+	    YM_CTRL_PORT |= _BV(YM_A0);  // A0 high (write register)
+	    write_ym(data);
+    }
+
+
+    public:
+    YM2612() {
+        /* Pins setup */
+        YM_CTRL_DDR |= _BV(YM_IC) | _BV(YM_CS) | _BV(YM_WR) | _BV(YM_RD) | _BV(YM_A0) | _BV(YM_A1);
+        //YM_DATA_DDR = 0xFF;
         YM_DATA_DDR = B11111100;
-	YM_MCLOCK_DDR |= _BV(YM_MCLOCK) | B00001100;
-	YM_CTRL_PORT |= _BV(YM_IC) | _BV(YM_CS) | _BV(YM_WR) | _BV(YM_RD); /* IC, CS, WR and RD HIGH by default */
-	YM_CTRL_PORT &= ~(_BV(YM_A0) | _BV(YM_A1)); /* A0 and A1 LOW by default */
+        YM_MCLOCK_DDR |= _BV(YM_MCLOCK) | B00001100;
+        YM_CTRL_PORT |= _BV(YM_IC) | _BV(YM_CS) | _BV(YM_WR) | _BV(YM_RD); /* IC, CS, WR and RD HIGH by default */
+        YM_CTRL_PORT &= ~(_BV(YM_A0) | _BV(YM_A1)); /* A0 and A1 LOW by default */
 
-/* F_CPU / 2 clock generation */
-	TCCR1A = _BV(COM1A0);            /* Toggle OCA1 on compare match */
-	TCCR1B = _BV(WGM12) | _BV(CS10); /* CTC mode with prescaler /1 */
-	TCCR1C = 0;                      /* Flag reset */
-	TCNT1 = 0;                       /* Counter reset */
-	OCR1A = 0;                       /* Divide base clock by two */
+        /* F_CPU / 2 clock generation */
+        TCCR1A = _BV(COM1A0);            /* Toggle OCA1 on compare match */
+        TCCR1B = _BV(WGM12) | _BV(CS10); /* CTC mode with prescaler /1 */
+        TCCR1C = 0;                      /* Flag reset */
+        TCNT1 = 0;                       /* Counter reset */
+        OCR1A = 0;                       /* Divide base clock by two */
 
-/* Reset YM2612 */
-	YM_CTRL_PORT &= ~_BV(YM_IC);
-	_delay_ms(10);
-	YM_CTRL_PORT |= _BV(YM_IC);
-	_delay_ms(10);
+        /* Reset YM2612 */
+        YM_CTRL_PORT &= ~_BV(YM_IC);
+        _delay_ms(10);
+        YM_CTRL_PORT |= _BV(YM_IC);
+        _delay_ms(10);
 
-/* YM2612 Test code */ 
-	setreg(0x22, 0x00); // LFO off
-	setreg(0x27, 0x00); // Note off (channel 0)
-	setreg(0x28, 0x01); // Note off (channel 1)
-	setreg(0x28, 0x02); // Note off (channel 2)
-	setreg(0x28, 0x04); // Note off (channel 3)
-	setreg(0x28, 0x05); // Note off (channel 4)
-	setreg(0x28, 0x06); // Note off (channel 5)
-	setreg(0x2B, 0x00); // DAC off
-	setreg(0x30, 0x71); // Detune; Multiple
-	setreg(0x34, 0x0D); // Detune; Multiple
-	setreg(0x38, 0x33); // Detune; Multiple
-	setreg(0x3C, 0x01); // DT1/MUL Detune; Multiple
-	setreg(0x40, 0x23); // Total level Operator 1 Ch1
-	setreg(0x44, 0x2D); // Total level Operator 2 Ch1
-	setreg(0x48, 0x26); // Total level Operator 3 Ch1
-	setreg(0x4C, 0x00); // Total level Operator 4 Ch1
-	setreg(0x50, 0x5F); // RS/AR Rate Scaling; Attack Rate
-	setreg(0x54, 0x99); // RS/AR Rate Scaling; Attack Rate
-	setreg(0x58, 0x5F); // RS/AR Rate Scaling; Attack Rate
-	setreg(0x5C, 0x94); // RS/AR Rate Scaling; Attack Rate
-	setreg(0x60, 0x05); // AM/D1R First Decay Rate; Amplitude Modulation
-	setreg(0x64, 0x05); // AM/D1R First Decay Rate; Amplitude Modulation
-	setreg(0x68, 0x05); // AM/D1R First Decay Rate; Amplitude Modulation
-	setreg(0x6C, 0x07); // AM/D1R First Decay Rate; Amplitude Modulation AM/D1R
-	setreg(0x70, 0x02); // D2R Secondary Decay Rate
-	setreg(0x74, 0x02); // D2R Secondary Decay Rate
-	setreg(0x78, 0x02); // D2R Secondary Decay Rate
-	setreg(0x7C, 0x02); // D2R Secondary Decay Rate
-	setreg(0x80, 0x11); // D1L/RR Secondary Amplitude; Release Rate
-	setreg(0x84, 0x11); // D1L/RR Secondary Amplitude; Release Rate
-	setreg(0x88, 0x11); // D1L/RR Secondary Amplitude; Release Rate
-	setreg(0x8C, 0xA6); // D1L/RR Secondary Amplitude; Release Rate
-	setreg(0x90, 0x00); // Proprietary
-	setreg(0x94, 0x00); // Proprietary
-	setreg(0x98, 0x00); // Proprietary
-	setreg(0x9C, 0x00); // Proprietary
-	setreg(0xB0, 0x30); // Feedback/algorithm
-	setreg(0xB4, 0xC0); // Both speakers on
-	setreg(0x28, 0x00); // Key off
-        //setreg(0xA4, 0x22); // Frequency MSB
-        //setreg(0xA0, 0x69); // Frequency LSB
+        /* YM2612 Test code */
+        setreg(0x22, 0x00, 0); // LFO off
+        operatorKey(0, 0);
+        operatorKey(1, 0);
+        operatorKey(2, 0);
+        operatorKey(3, 0);
+        operatorKey(4, 0);
+        operatorKey(5, 0);
+        setreg(0x2B, 0x00, 0); // DAC off
+        voice(0);
+        voice(1);
+        voice(2);
+        voice(3);
+        voice(4);
+        voice(5);
+        setreg(0x90, 0x00, 0); // Proprietary
+        setreg(0x94, 0x00, 0); // Proprietary
+        setreg(0x98, 0x00, 0); // Proprietary
+        setreg(0x9C, 0x00, 0); // Proprietary
+    }
+
+    
+    void operatorKey(byte channel, byte bitfield) {
+        if (bitfield)
+            digitalWrite(13, HIGH);
+        else
+            digitalWrite(13, LOW);
+	    setreg(0x28, ((bitfield & B1111) << 4) | channel, 0);
+    }
 
 
+    void frequency(byte channel, uint16_t pitch) {
+	    byte pitchMsb = pitch >> 8; //extract the most significant byte from the pitch word, shift the byte down and assign it to the pitchMSB byte variable
+	    byte pitchLsb = pitch & 0xFF; //mask out the most significant byte using 0xFF, then assign the least significant byte to pitchLSB
+	    byte lsbReg =  channel <= 5 ? 0xA0 : 0xAC; //if higher than 5 then use the special mode registers
+	    byte chanOffset = channel % 3; //channels are in sequence, and 012 overlap 345
+	    byte bank = 3 <= channel && channel <= 5; // bank is 0 unless channel is 3 4 5
+	    setreg(lsbReg + chanOffset + 4, pitchMsb, bank); //pitch MSB first, MSB register is LSB+4
+	    setreg(lsbReg + chanOffset, pitchLsb, bank);
+    }
 
-  blinkTest(3,200,200);
-  //blinkTest(3,400,200);
-  //blinkTest(3,200,200);
+
+    void level(byte channel, byte val)
+    {
+        if (channel < 6) {
+	        byte chanOffset = channel % 3; //channels are in sequence, and 012 overlap 345
+	        byte bank = 3 <= channel && channel <= 5; // bank is 0 unless channel is 3 4 5
+            setreg(0x40 + chanOffset, val, bank); // Operator 1
+/*            if (slotOperators[channel] | YM2612_OP1)
+	            setreg(0x40 + chanOffset, val, bank); // Operator 1
+            if (slotOperators[channel] | YM2612_OP2)
+	            setreg(0x44 + chanOffset, val, bank); // Operator 2
+            if (slotOperators[channel] | YM2612_OP3)
+	            setreg(0x48 + chanOffset, val, bank); // Operator 3
+            if (slotOperators[channel] | YM2612_OP4)
+	            setreg(0x4C + chanOffset, val, bank); // Operator 4
+*/
+        }
+    }
+
+
+    void voice(byte channel)
+    {
+	    byte chanOffset = channel % 3; //channels are in sequence, and 012 overlap 345
+	    byte bank = 3 <= channel && channel <= 5; // bank is 0 unless channel is 3 4 5
+	    setreg(0x28 + chanOffset, 0x00, bank); // Key off
+	    setreg(0x30 + chanOffset, 0x71, bank); // Detune; Multiple
+	    setreg(0x34 + chanOffset, 0x0D, bank); // Detune; Multiple
+	    setreg(0x38 + chanOffset, 0x33, bank); // Detune; Multiple
+	    setreg(0x3C + chanOffset, 0x01, bank); // DT1/MUL Detune; Multiple
+	    setreg(0x40 + chanOffset, 0x23, bank); // Total level Operator 1
+	    setreg(0x44 + chanOffset, 0x2D, bank); // Total level Operator 2
+	    setreg(0x48 + chanOffset, 0x26, bank); // Total level Operator 3
+	    setreg(0x4C + chanOffset, 0x00, bank); // Total level Operator 4
+	    setreg(0x50 + chanOffset, 0x5F, bank); // RS/AR Rate Scaling; Attack Rate
+	    setreg(0x54 + chanOffset, 0x99, bank); // RS/AR Rate Scaling; Attack Rate
+	    setreg(0x58 + chanOffset, 0x5F, bank); // RS/AR Rate Scaling; Attack Rate
+	    setreg(0x5C + chanOffset, 0x94, bank); // RS/AR Rate Scaling; Attack Rate
+	    setreg(0x60 + chanOffset, 0x05, bank); // AM/D1R First Decay Rate; Amplitude Modulation
+	    setreg(0x64 + chanOffset, 0x05, bank); // AM/D1R First Decay Rate; Amplitude Modulation
+	    setreg(0x68 + chanOffset, 0x05, bank); // AM/D1R First Decay Rate; Amplitude Modulation
+	    setreg(0x6C + chanOffset, 0x07, bank); // AM/D1R First Decay Rate; Amplitude Modulation AM/D1R
+	    setreg(0x70 + chanOffset, 0x02, bank); // D2R Secondary Decay Rate
+	    setreg(0x74 + chanOffset, 0x02, bank); // D2R Secondary Decay Rate
+	    setreg(0x78 + chanOffset, 0x02, bank); // D2R Secondary Decay Rate
+	    setreg(0x7C + chanOffset, 0x02, bank); // D2R Secondary Decay Rate
+	    setreg(0x80 + chanOffset, 0x11, bank); // D1L/RR Secondary Amplitude; Release Rate
+	    setreg(0x84 + chanOffset, 0x11, bank); // D1L/RR Secondary Amplitude; Release Rate
+	    setreg(0x88 + chanOffset, 0x11, bank); // D1L/RR Secondary Amplitude; Release Rate
+	    setreg(0x8C + chanOffset, 0xA6, bank); // D1L/RR Secondary Amplitude; Release Rate
+        feedbackAlgorithm(channel, 6, 0);
+	    setreg(0xB4 + chanOffset, 0xC0, bank); // Both speakers on
+    }
+
+
+//keep track of which operators are in algorithm slots
+    void feedbackAlgorithm(byte channel, byte fb, byte alg) {
+        if (channel < 6) {
+	        byte chanOffset = channel % 3; //channels are in sequence, and 012 overlap 345
+	        byte bank = 3 <= channel && channel <= 5; // bank is 0 unless channel is 3 4 5
+            alg &= B00000111;
+            setreg(0xB0 + chanOffset, ((fb & B00000111) << 3) | alg, bank); //feedback is 3 bits offset 3, algorithm is 3 bits no offset
+/* algorithm : operators
+    0-3 : 4
+    4   : 2,4
+    5,6 : 2,3,4
+    7   : 1,2,3,4
+*/
+            slotOperators[channel] = YM2612_OP4
+                | (alg >= 5 ? YM2612_OP3 : 0)
+                | (alg >= 4 ? YM2612_OP2 : 0)
+                | (alg == 7 ? YM2612_OP1 : 0);
+        }
+    }
+};
+
+
+class Megasynth {
+    YM2612 ym;
+
+    public:
+    Megasynth() {
+        pinMode(13,OUTPUT);
+	    digitalWrite(13,LOW);
+        ym = YM2612();
+        //blinkTest(3,200,200);
+    }
+
+
+    void noteOn(byte channel, byte note, byte velocity) {
+	    if (channel <= 4 || (10 <= channel && channel <= 12 )) {
+		    ym.frequency(channel, pgm_read_word(&PITCHTABLE[note])); //use pitch table in tables.h to get the appropriate pitch word
+		    if (channel <=4)
+    		    ym.operatorKey(channel, YM2612_OP1 | YM2612_OP2 | YM2612_OP3 | YM2612_OP4); //enable ALL the operators
+                ym.level(channel, velocity);
+	    }
+    }
+
+
+    void noteOff(byte channel, byte note, byte velocity) {
+	    if (channel <= 4)
+		    ym.operatorKey(channel, 0); //disable ALL the operators
+    }
+};
+
+
+Megasynth synth = Megasynth();
+
+
+void setup() {
+    /* activate MIDI Serial input - Gopal */
+    Serial.begin(38400);
+
+    delay(200);
+    blinkTest(3,200,200);
+    //blinkTest(3,400,200);
+    //blinkTest(3,200,200);
 }
 
 /*
@@ -152,57 +263,46 @@ void velocityMap() {
 */
 
 
-
-void MIDIinput() {
-  do {    
-    if (Serial.available()) {
-      statusByte = Serial.read(); // get first byte of MIDI data from computer
-      statusCommand = ((statusByte & B11110000) >> 4);
-      MIDIChannel = statusByte & B00001111;
-
-      switch (statusCommand) {
-        case 0x9: //NoteOn
-          /*if (!((databyte1 = Serial.read()) & B10000000) && !((databyte2 = Serial.read()) & B10000000)) {  
-          }*/        
-          //blinkTest(1,10,1);
-          databyte1 = Serial.read();  //note value
-          databyte2 = Serial.read();   //note velocity
-          pitch = pgm_read_word(&PITCHTABLE[databyte1]); //call the pitch table in tables.h to get the appropriate pitch word (word=2 bytes)
-          pitchMSB = (pitch & 0x3F00) >> 8; //extract the most significant byte from the pitch word, shift the byte down and assign it to the pitchMSB byte variable
-          pitchLSB = pitch & 0xFF; //mask out the most significant byte using 0xFF, then assign the least significant byte to pitchLSB
-          setreg(0xA4, pitchMSB); //send the note frequency MSB to the Block/Frequency MSB register on the YM2612 the note value
-          setreg(0xA0, pitchLSB); //send the note frequency LSB to the Frequency LSB register on the YM2612
-          setreg(0x28, 0xF0); // Play a note ON; need to map this value to the correct algorithm "Slot(s)"
-          
-          break;
-
-        case 0x8: //NoteOff
-         //blinkTest(3,100,10);
-         setreg(0x28, 0x00); // Stop the note
-         databyte1 = Serial.read();  //note value
-         databyte2 = Serial.read();   //note velocity
-          break;
-
-        case 0xB: //Continuous Controller (CC)
-         // blinkTest(1, 100, 100);
-         databyte1 = Serial.read(); // CC number
-         databyte2 = Serial.read(); // CC value
-/*
-           switch (databyte1)
-             case 0x14: // testing CC information
-             blinkTest(2, 10, 50);
-             setreg(0xB0, (databyte2 & B00000111)); //Changing the algorithm
-             break;
-*/
-         break;
-        default:
-        break;          
-        }
-      }
-  }
-  while (Serial.available() >= 2);//when at least three bytes available
-}
-
 void loop() {
-  MIDIinput();
+    while (Serial.available() >= 2) {
+        byte statusByte;
+        byte statusCommand;
+        byte MIDIChannel;
+        byte databyte1; //Register and offset
+        byte databyte2; //Actual value
+        statusByte = Serial.read();
+        statusCommand = (statusByte & B11110000) >> 4;
+        MIDIChannel = statusByte & B00001111;
+
+        switch (statusCommand) {
+            case 0x9:
+            databyte1 = Serial.read(); //note value
+            databyte2 = Serial.read(); //note velocity
+            synth.noteOn(MIDIChannel, databyte1, databyte2);
+            break;
+
+            case 0x8:
+            databyte1 = Serial.read(); //note value
+            databyte2 = Serial.read(); //note velocity
+            synth.noteOff(MIDIChannel, databyte1, databyte2);
+            break;
+
+            case 0xB: //Continuous Controller (CC)
+            databyte1 = Serial.read(); // CC number
+            databyte2 = Serial.read(); // CC value
+/*
+            switch (databyte1) {
+                case 0x14: // testing CC information
+                blinkTest(2, 10, 50);
+                setreg(0xB0, (databyte2 & B00000111), 0); //Changing the algorithm
+                break;
+            }               
+*/
+            break;
+
+            default:
+            break;          
+        }
+    }
 }
+
